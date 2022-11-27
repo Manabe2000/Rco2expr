e7312ce6cdd4464c5dfb866eebb5c4d7f0df309f
manabe <manabe2022@s.okayama-u.ac.jp>
2022-11-28 02:39:18 +0900

    Modified range

--- estimate_k_in_attendance.R
+++ estimate_k_in_attendance.R
@@ -13,13 +13,13 @@ START_CO2 <- df1[1,3]
 END_CO2 <- df1[RANGE,3]
 X <- c()
 for (i in 1:RANGE) {
-    X <- c(X,as.duration(interval(df[1,1],df[i,1])))
+    X <- c(X,as.duration(interval(df1[1,1],df1[i,1])))
 }
 Y1 <- df1$field1
 cols <- c("black","green")
 result=nls(Y1~A*(1-exp(-(k*X)))+START_CO2*(1-exp(-(k*X)))+START_CO2*(exp(-(k*X))),start=list(A=300,k=20/60000))
 sink("result.csv",append=T)
-paste(pic_str,coef(result)[1],coef(result)[2],coef(result)[1]/coef(result)[2],START_CO2,RANGE,sep=",")
+paste(pic_str,coef(result)[1],coef(result)[2],coef(result)[1]/coef(result)[2],START_CO2,as.duration(interval(df1[1,1],df1[RANGE,1])),sep=",")
 sink()
 plot(X,Y1,type="l",col=cols[1],xlim=c(1,X[RANGE]),ylim=c(300,1000),xlab="経過時間[s]",ylab="co2[ppm]")
 par(new=T)
